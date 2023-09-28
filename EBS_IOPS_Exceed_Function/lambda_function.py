846762d021f0bb26a9ed2f931ab3e7c141b461bb
Bryan Chen <60024199+bryancwh@users.noreply.github.com>
2023-09-28 20:50:02 +0800

    Update lambda_function.py

--- EBS_IOPS_Exceed_Function/lambda_function.py
+++ EBS_IOPS_Exceed_Function/lambda_function.py
@@ -4,15 +4,15 @@ import math
 
 def lambda_handler(event, context):
     
-    print(event)
-    # Add comments
-    
-    # Get notification metadata
+    # Get volumeId and alarmName from event metadata
     eventMessage=json.loads(event['Records'][0]['Sns']['Message'])
     volumeid=eventMessage['Trigger']['Metrics'][1]['MetricStat']['Metric']['Dimensions'][0]['value']
-    alarmName=eventMessage['AlarmName']
-    
-    # Modify EBS Volume
+    alarmName=eventMessage['AlarmName'] 
+
+
+    # Modify the EBS volume
+
+    # Get volume metadata from ec2 client 
     client = boto3.client('ec2')
     
     metadata = client.describe_volumes(
@@ -27,11 +27,13 @@ def lambda_handler(event, context):
     throughput=metadata['Volumes'][0]['Throughput']
     multiattachenabled=metadata['Volumes'][0]['MultiAttachEnabled']
 
+    # Define increment of IOPS & Size
     iops+=500
     size+=5
     
     # new_size=math.ceil(iops/500)
-    
+
+    # EBS volume modification
     response = client.modify_volume(
         DryRun=False,
         VolumeId=volumeid,
@@ -46,7 +48,10 @@ def lambda_handler(event, context):
     
     print("Successfully increased Volume Iops to: " + str(iops))
 
-    # Modify CW Alarm Threshold
+
+    # Modify the CloudWatch Alarm threshold
+
+    # Get alarm metadata from CloudWatch client
     clientCW = boto3.client('cloudwatch')
 
     alarmMetadata = clientCW.describe_alarms(
@@ -62,8 +67,10 @@ def lambda_handler(event, context):
     metrics=alarmMetadata['MetricAlarms'][0]['Metrics']
     threshold= alarmMetadata['MetricAlarms'][0]['Threshold']
     
+    # Define increment of CW alarm threshold
     threshold+=500
     
+    # CW alarm threshold modification 
     cloudwatchModification = clientCW.put_metric_alarm(
         AlarmName=alarmname,
         AlarmActions=alarmactions,
